9fd7a42cba1ad93734780b760319d2f2e07092d9
Obi-wan77 <mattpil77@hotmail.fr>
2017-12-26 18:37:40 +0100

    addition method

--- operations.cpp
+++ operations.cpp
@@ -72,103 +72,6 @@ char** segmentMessage(char* message, int length) {
     return result;
 }
 
-/*char* And(char X[4], char Y[4]) {
-
-    char* result = newString(4);
-
-    for (unsigned int i = 0; i < 4; i++) {
-        result[i] = X[i] & Y[i];
-    }
-
-    return result;
-}
-
-char* Or(char X[4], char Y[4]) {
-
-    char* result = newString(4);
-
-    for (unsigned int i = 0; i < 4; i++) {
-        result[i] = X[i] | Y[i];
-    }
-
-    return result;
-}
-
-char* Not(char X[4]) {
-
-    char* result = newString(4);
-
-    for (unsigned int i = 0; i < 4; i++) {
-        result[i] = ~X[i];
-    }
-
-    return result;
-}
-
-char* Xor(char X[4], char Y[4]) {
-
-    char* result = newString(4);
-
-    for (unsigned int i = 0; i < 4; i++) {
-        result[i] = X[i] ^ Y[i];
-    }
-
-    return result;
-}
-
-char* F(char X[4], char Y[4], char Z[4]) {
-
-    char* XY = And(X, Y);
-    char* NotX = Not(X);
-    char* NotXZ = And(NotX, Z);
-
-    char* result = Or(XY, NotXZ);
-
-    free(XY);
-    free(NotX);
-    free(NotXZ);
-
-    return result;
-
-}
-
-char* G(char X[4], char Y[4], char Z[4]) {
-
-    char* XZ = And(X, Z);
-    char* NotZ = Not(Z);
-    char* YNotZ = And(Y, NotZ);
-
-    char* result = Or(XZ, YNotZ);
-
-    free(XZ);
-    free(NotZ);
-    free(YNotZ);
-
-    return result;
-}
-
-char* H(char X[4], char Y[4], char Z[4]) {
-
-    char* XxorY = Xor(X, Y);
-    char* result = Xor(XxorY, Z);
-
-    free(XxorY);
-
-    return result;
-}
-
-char* I(char X[4], char Y[4], char Z[4]) {
-
-    char* NotZ = Not(Z);
-    char* XNotZ = Or(X, NotZ);
-    char* result = Xor(Y, XNotZ);
-
-    free(NotZ);
-    free(XNotZ);
-
-    return result;
-}*/
-
 // AND Operation when we work with a char representing a bit
 char* AndBit(char X[32], char Y[32]) {
 
@@ -282,6 +185,22 @@ char *HexToBinary(unsigned char X[4]){
             result[i*8 + j] = temp%2;
             temp /= 2;
         }
+    }
+    return result;
+}
+
+char *AdditionBit(char X[32],char Y[32]){
+
+    char *result = newString(32);
+    int retenue = 0;
+
+    for(int i = 31;i>=0;i--){
+        int resultAddition = X[i] + Y[i] + retenue;
+        result[i] = resultAddition%2;
+        if(resultAddition>1)
+            retenue = 1;
+        else
+            retenue = 0;
     }
     return result;
 }
--- operations.h
+++ operations.h
@@ -6,11 +6,6 @@ int padding(int passwordLen);
 int numberOfBlocks(int passwordLen);
 void intToHex(int numberBlocks,int passwordLen,char* blocks);
 char** segmentMessage(char* message, int length);
-/*
-char* F(char X[4], char Y[4], char Z[4]);
-char* G(char X[4], char Y[4], char Z[4]);
-char* H(char X[4], char Y[4], char Z[4]);
-char* I(char X[4], char Y[4], char Z[4]);*/
 
 char* FBit(char X[32], char Y[32], char Z[32]);
 char* GBit(char X[32], char Y[32], char Z[32]);
@@ -18,5 +13,6 @@ char* HBit(char X[32], char Y[32], char Z[32]);
 char* IBit(char X[32], char Y[32], char Z[32]);
 
 char *HexToBinary(unsigned char X[4]);
+char *AdditionBit(char X[32],char Y[32]);
 
 #endif // OPERATIONS_H_INCLUDED
